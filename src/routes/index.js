9d0da56891c9222de7f0ae73fb51f0005fe7cf07
John Wojtkielewicz <john.wojtk@gmail.com>
2020-03-08 19:50:34 -0400

    Installed dotenv, and added corresponding template.env, which will be useful for many future enhancements. package.json auto-formatted by npm. Moved error router out of app.js and user router out of routes/index.js and each into their own module. Added unifying index.js for models, as is the case for routes. (#20)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,23 +1,12 @@
-var User = require('../models/User');
-
 var express = require('express');
+var errorRouter = require('./error');
+var userRouter = require('./user');
+
+// Instantiate and apply sub-routers
 var router = express.Router();
+router.use('/user', userRouter);
 
-/* GET */
-router.get('/', function(req, res, next) {
-  res.send('Hello world!');
-});
-
-/* GET Users */
-router.get('/user', function(req, res, next) {
-  var users = [];
-  
-  users.push(new User({name: "John W"}));
-  users.push(new User({name: "Steph Z"}));
-  users.push(new User({name: "Cat"}));
-  users.push(new User({name: "Lily"}));
-  
-  res.send(users);
-});
+// Error handler must come last
+router.use(errorRouter);
 
 module.exports = router;
