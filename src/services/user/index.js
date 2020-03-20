dc33fdced74e8366f1f403ddddd14516fdca7c10
John Wojtkielewicz <john.wojtk@gmail.com>
2020-03-19 20:02:48 -0400

    Added endpoint and service method for a single get. Revised some error handling.

--- src/services/user/index.js
+++ src/services/user/index.js
@@ -21,6 +21,18 @@ class UserService extends DataService {
     return feedResponse.resources.map(user => new User(user));
   }
   
+  async get(userId) {
+    const item = this.container.item(userId);
+    const itemResponse = await item.read();
+    const resource = itemResponse.resource;
+    
+    if (itemResponse.statusCode !== 404 && resource === undefined) {
+      throw 'The database responded in an unexpected manner.';
+    }
+    
+    return resource;
+  }
+  
   /*
   async create(user) {
     const itemResponse = await this.container.items.create({
